2eafefb9cac94a39c7911ab1e2ba7d1490fe563d
sdwck <sdwcktarakanov@gmail.com>
2025-08-13 18:38:50 +0300

    feat: replace message icon with updated filled version

--- src/shared/components/icons.ts
+++ src/shared/components/icons.ts
@@ -7,11 +7,11 @@ type PathAttributes = {
   'stroke-linecap'?: string;
 };
 
-function createSvgElement(paths: PathAttributes[]): SVGElement {
+function createSvgElement(paths: PathAttributes[], width = 24, height = 24): SVGElement {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('viewBox', '0 0 24 24');
-    svg.setAttribute('height', '24');
-    svg.setAttribute('width', '24');
+    svg.setAttribute('height', height.toString());
+    svg.setAttribute('width', width.toString());
     svg.setAttribute('focusable', 'false');
     svg.style.pointerEvents = 'none';
     svg.style.display = 'block';
@@ -77,23 +77,19 @@ export function createCloseIcon(): SVGElement {
 export function createStrokedMessageIcon(): SVGElement {
   return createSvgElement([
     {
-      d: 'M20 2H4a2 2 0 0 0-2 2v16l4-4h14a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z',
-      fill: 'none',
-      stroke: 'var(--yt-spec-icon-active-other, #fff)',
-      'stroke-width': '1.5',
-      'stroke-linejoin': 'round',
-      'stroke-linecap': 'round',
+      d: 'm.71,24c-.09,0-.17-.02-.25-.05-.27-.1-.45-.37-.45-.66V3.22C0,1.44,1.33,0,2.96,0h18.07c1.64,0,2.97,1.44,2.97,3.22v12.55c0,1.77-1.33,3.22-2.97,3.22H5.54l-4.31,4.79c-.14.15-.33.23-.52.23ZM2.96,1.41c-.86,0-1.55.81-1.55,1.8v18.24l3.29-3.65c.13-.15.32-.23.52-.23h15.81c.86,0,1.55-.81,1.55-1.8V3.22c0-.99-.7-1.8-1.55-1.8H2.96Z',
+      fill: 'var(--yt-spec-icon-active-other, #fff)'
     }
-  ]);
+  ], 20, 20);
 }
 
 export function createFilledMessageIcon(): SVGElement {
   return createSvgElement([
     {
-      d: 'M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z',
+      d: 'm20.93,0H3.07C1.38,0,0,1.52,0,3.38v19.69c0,.37.2.71.52.85.1.05.21.07.32.07.22,0,.43-.09.59-.27l4.22-4.65h15.28c1.69,0,3.07-1.52,3.07-3.38V3.38c0-1.87-1.38-3.38-3.07-3.38Zm-2.23,14.46H5.3v-2.46h13.4v2.46Zm0-3.69H5.3v-2.46h13.4v2.46Zm0-3.69H5.3v-2.46h13.4v2.46Z',
       fill: 'var(--yt-spec-icon-active-other, #fff)',
     }
-  ]);
+  ], 20, 20);
 }
 
 export function createBackArrowIcon(): SVGElement {
